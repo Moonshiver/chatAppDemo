88b20d951a548bb14a791500fd351e81119c5fb5
tiaferre <tiagoarf420@gmail.com>
2025-02-20 20:26:47 +0000

    server ws

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("teste");
+import { WebSocket, WebSocketServer } from "ws";
+import { createServer } from "http";
+
+const server = createServer();
+server.listen(8888, () => {
+  console.log("im up");
+});
+
+const wss = new WebSocketServer({ server: server });
+
+wss.on("connection", (socket, req) => {
+  socket.on("message", (data, isBinary) => {
+    wss.clients.forEach((client) => {
+      if (client != socket && client.readyState == WebSocket.OPEN) {
+        client.send(data.toString());
+      }
+    });
+  });
+});
